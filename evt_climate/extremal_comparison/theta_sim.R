9880ddb28f1d2d49627d815328049093e367cb9f
mickey <mickey.a.warner@gmail.com>
2017-06-29 14:30:06 -0700

    yes

--- evt_climate/extremal_comparison/theta_sim.R
+++ evt_climate/extremal_comparison/theta_sim.R
@@ -23,80 +23,90 @@ library(mwEVT)
 
 R = 10
 n = 1000
-B = 100
+B = 200
 
-fb.m = matrix(0, B, R+1)
-fb.v = matrix(0, B, 2)
-
-sb.m = matrix(0, B, R+1)
-sb.v = matrix(0, B, 2)
 
 ### Simulation: Frechet
 set.seed(1)
+
+theta = 0.10
+
+fb.v = matrix(0, B, 2)
+fb.r = double(B)
+sb.v = matrix(0, B, 2)
+sb.r = double(B)
+
 for (k in 1:B){
-    for (theta in c(0.15, 0.85)){
-        ww = matrix(-1/log(runif(n*R)), n, R)
-        y = matrix(0, n, R)
-        y[1,] = ww[1,] / theta
-        for (i in 2:n)
-            y[i,] = apply(rbind((1-theta)*y[i-1,], ww[i,]), 2, max)
+    ww = matrix(-1/log(runif(n*R)), n, R)
+    y = matrix(0, n, R)
+    y[1,] = ww[1,] / theta
+    for (i in 2:n)
+        y[i,] = apply(rbind((1-theta)*y[i-1,], ww[i,]), 2, max)
 
-        ### For split up sequences
-        uu = quantile(y, 0.95)
-    #   K = 12
-    #   uu = seq(quantile(y, 0.80), quantile(y, 0.97), length = K)
-        N = sapply(uu, function(x) sum(y > x))
+    ### For split up sequences
+    uu = quantile(y, 0.90)
+    # K = 12
+    # uu = seq(quantile(y, 0.80), quantile(y, 0.97), length = K)
+    N = sapply(uu, function(x) sum(y > x))
 
-        # N = round(seq(1000, 10, length = K))
-        # uu = sapply(N, function(x) quantile(y, 1 - x/n))
+    # N = round(seq(1000, 10, length = K))
+    # uu = sapply(N, function(x) quantile(y, 1 - x/n))
 
-        up = mean(y <= uu)
-            
-        ferro_ba = theta_hier(y, uu, likelihood = "ferro",
-            nburn = 10000, nmcmc = 10000)
-        tmp = c(ferro_ba$keep, R+1)
-        fb.m[j, tmp] = colMeans(ferro_ba$mcmc)
-        fb.m[j, -tmp] = NA
-        fb.v[j,] = quantile(ferro_ba$mcmc[,which(tmp == R+1)], c(0.025, 0.975))
+    up = mean(y <= uu)
+        
+    ferro_ba = theta_hier(y, uu, likelihood = "ferro",
+        nburn = 10000, nmcmc = 10000)
+    tmp = c(ferro_ba$keep, R+1)
+#   fb.v[k,] = quantile(ferro_ba$mcmc[,which(tmp == R+1)], c(0.025, 0.975))
+    fb.v[k,] = hpd_mult(ferro_ba$mcmc[,which(tmp == R+1)], force_uni = TRUE)
+    
 
-        # RMSE
-        sqrt(mean((ferro_ba$mcmc[,which(tmp == R+1)] - theta)^2))
+    # RMSE
+    fb.r[k] = sqrt(mean((ferro_ba$mcmc[,which(tmp == R+1)] - theta)^2))
 
-        suveges_ba = theta_hier(y, uu, likelihood = "suveges",
-            nburn = 10000, nmcmc = 10000)
-        tmp = c(suveges_ba$keep, R+1)
-        sb.m[j, tmp] = colMeans(suveges_ba$mcmc)
-        sb.m[j, -tmp] = NA
-        sb.v[j,] = quantile(suveges_ba$mcmc[,which(tmp == R+1)], c(0.025, 0.975))
+    suveges_ba = theta_hier(y, uu, likelihood = "suveges",
+        nburn = 10000, nmcmc = 10000)
+    tmp = c(suveges_ba$keep, R+1)
+#   sb.v[k,] = quantile(suveges_ba$mcmc[,which(tmp == R+1)], c(0.025, 0.975))
+    sb.v[k,] = hpd_mult(suveges_ba$mcmc[,which(tmp == R+1)], force_uni = TRUE)
 
-        pdf(paste0("./figs/sim_frechet_hier_", as.character(theta),"_",n*R,"_",R, ".pdf"),
-            width = 12, height = 8)
-        par(mfrow = c(1, 2))
-        mains = c("Ferro Bayes", "Suveges Bayes")
-        cols = c("blue", "orange")
-        for (j in 1:2){
-            mm = switch(j,
-                "1" = fb.m,
-                "2" = sb.m)
-            vv = switch(j,
-                "1" = fb.v,
-                "2" = sb.v)
-            plot(uu, mm[,R+1], type = 'b', pch = 15, col = col_mult(cols[j], 'gray75'),
-                xlab = "Threshold", ylab = expression(theta), bty = 'n',
-                ylim = c(0, 1))
-            matplot(uu, mm[,-(R+1)], add = TRUE, type = 'l',
-                col = col_fade(cols[j], 0.25), lty = 1)
-            lines(uu, vv[,1], col = col_fade(cols[j], 0.75))
-            lines(uu, vv[,2], col = col_fade(cols[j], 0.75))
-            title(main = mains[j], line = 2.5)
-            tmp = axTicks(1)
-        #   axis(3, at = tmp, labels = sapply(tmp, function(x) sum(y > x))) 
-            axis(3, at = tmp, labels = round(sapply(tmp, function(x) mean(y < x)), 3) )
-            rm(tmp)
-            if (exists("theta"))
-                abline(h = theta, lty = 2)
-            }
-        par(mfrow = c(1, 1))
-        dev.off()
-        }
+    # RMSE
+    sb.r[k] = sqrt(mean((suveges_ba$mcmc[,which(tmp == R+1)] - theta)^2))
+
     }
+
+
+# Coverage
+mean(apply(fb.v, 1, function(x) x[1] < theta && x[2] > theta))
+mean(apply(sb.v, 1, function(x) x[1] < theta && x[2] > theta))
+
+plot(0, type = 'n', xlim = c(1, B), ylim = c(0, 1))
+segments(x0 = 1:B, x1 = 1:B, y0 = fb.v[,1], y1 = fb.v[,2])
+abline(h = theta)
+
+plot(0, type = 'n', xlim = c(1, B), ylim = c(0, 1))
+segments(x0 = 1:B, x1 = 1:B, y0 = sb.v[,1], y1 = sb.v[,2])
+abline(h = theta)
+
+plot(0, type = 'n', xlim = c(1, B/1), ylim = c(0, 1))
+segments(x0 = 1:B, x1 = 1:B, y0 = fb.v[,1], y1 = fb.v[,2], col = rgb(0,0,1,0.5))
+points(apply(fb.v, 1, mean), col = rgb(0,0,1,0.5))
+segments(x0 = 1:B+0.0, x1 = 1:B+0.0, y0 = sb.v[,1], y1 = sb.v[,2], col = rgb(1,0,0,0.5))
+points(apply(sb.v, 1, mean), col = rgb(1,0,0,0.5))
+abline(h = theta)
+
+# RMSE
+mean(fb.r)
+mean(sb.r)
+
+median(fb.r)
+median(sb.r)
+
+plot(density(fb.r))
+lines(density(sb.r), col = 'red')
+
+plot(density(apply(fb.v, 1, diff)))
+lines(density(apply(sb.v, 1, diff)), col = 'red')
+
+plot(apply(fb.v, 1, diff), apply(sb.v, 1, diff))
+cor(apply(fb.v, 1, diff), apply(sb.v, 1, diff))
